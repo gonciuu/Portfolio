d2c1b9d6b06472ded03edbacfed8cebfdea54bbe
Kacper Wojak <k.wojak@medicalgorithmics.com>
2022-12-31 15:50:18 +0100

    Skills: items aligment

--- src/components/Skills/Cards/UiDesign.tsx
+++ src/components/Skills/Cards/UiDesign.tsx
@@ -2,10 +2,9 @@ import SkillCard from '../common/SkillCard'
 
 export default function UiDesign() {
   return (
-    <SkillCard className="col-span-12 row-span-2 sm:col-span-6 lg:col-span-3">
-      <h3 className="text-3xl font-bold text-white">
-        UI/UX <br />
-        <span className="text-[#22E596]">Design</span> <br />
+    <SkillCard className="col-span-12 row-span-2 items-center sm:col-span-6 lg:col-span-3">
+      <h3 className="text-center text-3xl font-bold text-white">
+        UI/UX <span className="text-[#22E596]">Design</span> <br />
         Experience
       </h3>
       <div className="mt-12 grid grid-cols-2 grid-rows-2 gap-4">
